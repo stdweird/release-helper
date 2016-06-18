59a6ecdac844895d37d60df3a694e719669cb7b2
stdweird <stijn.deweirdt@ugent.be>
2016-06-18 08:33:54 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import glob
+from setuptools import setup
+
+
+SETUP = {
+    'name': 'release-helper',
+    'version': '0.1.0',
+    'author': 'James Adams',
+    'author_email': 'james.adams@stfc.ac.uk',
+    'license': 'APL',
+    'packages' : ['release_helper'],
+    'package_dir': {'': 'lib'},
+    'scripts': glob.glob('bin/*.py') + glob.glob('bin/*.sh'),
+    'install_requires': [
+        'PyGithub',
+    ],
+    'test_suite': 'test',
+    'data_files': [
+        ('data', glob.glob('data/*html') + glob.glob('data/*example')),
+    ],
+    'zip_safe': False,
+}
+
+if __name__ == '__main__':
+    setup(**SETUP)
